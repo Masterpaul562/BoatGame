7faed2f79562bc1a2ed6e9918bff7557a6e125ab
Masterpaul562 <9652749@graniteschools.org>
2025-09-24 16:34:00 -0600

    fish setup
    not much, just the foundation of system

---
+++ Assets/Scripts/World/Fish.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+   private void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/World/FishSpawner.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject fishPrefab;
+    List<GameObject> fish = new List<GameObject>();
+
+
+
+    public void SpawnFish()
+    {
+
+    }
+}
