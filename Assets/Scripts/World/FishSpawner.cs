fad23f1cd710acad99b71cbbc7fe5be9bcae98af
Masterpaul562 <9652749@graniteschools.org>
2025-09-25 16:47:20 -0600

    fishy go spawn spawn

--- Assets/Scripts/World/FishSpawner.cs
+++ Assets/Scripts/World/FishSpawner.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 
 public class FishSpawner : MonoBehaviour
 {
+    [SerializeField] List<GameObject> fish = new List<GameObject>();
     [SerializeField] private GameObject fishPrefab;
     [SerializeField] private GameObject bobber;
     [SerializeField] private EnterFishing enterFishing;
-    List<GameObject> fish = new List<GameObject>();
+    private bool hasSpawned;
+    public int numOfFish;
 
-    private void Update() 
+    private void Update()
     {
- if (enterFishing.isFishing) 
-{
-    SpawnFish();
-}
+        int childNum = Random.Range(0,3);
+        Debug.Log(childNum);
+        
+        if (enterFishing.isFishing)
+        {
+            if (!hasSpawned)
+            {
+                SpawnFish();
+            }
+
+        }
+        else if (enterFishing.isFishing == false){
+            for (int i = 0; i < fish.Count; i++)
+            {
+                Destroy(fish[i]);
+                fish.RemoveAt(i);
+            }
+        }
+
 
     }
 
     public void SpawnFish()
     {
-       // Vector2 position = new Vector2 (random(0,10),random(0,10));
-    //Instantiate(fishPrefab,0,0, Quaternion.identity);
+        hasSpawned = true;
+
+        for (int i = 0; i < numOfFish; i++)
+        {
+
+            int childNum = Random.Range(0, 3);
+            Vector2 spawnChild = transform.GetChild(childNum).position;
+
+            Vector2 spawnLocation = new Vector2(spawnChild.x + Random.Range(-4, 4), spawnChild.y + Random.Range(-2, 2));
+
+            var fishs = Instantiate(fishPrefab,spawnLocation, Quaternion.identity);
+            fish.Add(fishs);
+        }
+
 
     }
 }
