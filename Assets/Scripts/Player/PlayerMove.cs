ea4865ae30fdc2319deb78bba525213983164111
Masterpaul562 <9652749@graniteschools.org>
2025-10-20 17:19:47 -0600

    my boi walking and idling it SO good

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -8,12 +8,15 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float horizontalInput;
     [SerializeField] private EnterFishing enterFScript;
+    [SerializeField] private bool isFacingRight;
+    [SerializeField] private Animator animator;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         enterFScript = GetComponent<EnterFishing>();
+        animator = GetComponent<Animator>();
     }
 
    
@@ -28,6 +31,8 @@ public class PlayerMove : MonoBehaviour
         {
             horizontalInput = Input.GetAxisRaw("Horizontal");
         }
+        Flip();
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
     }
     private void FixedUpdate()
     {
@@ -37,5 +42,18 @@ public class PlayerMove : MonoBehaviour
     private void Move()
     {
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+    }
+    private void Flip()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+            animator.SetBool("isFacingRight", isFacingRight);
+        }
+        
+
     }
 }
