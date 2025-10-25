ff1b136242156bcf998db5b81b57aa88daa4da91
Masterpaul562 <9652749@graniteschools.org>
2025-10-25 15:19:18 -0600

    monster spawining

--- Assets/Scripts/World/Monster.cs
+++ Assets/Scripts/World/Monster.cs
@@ -5,13 +5,60 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     public bool shouldAppear;
+    public int spawnChance;
     public float spawnTimer;
     public int feedAmount;
-    private int feedAmountMin, feedAmountMax ;
-
-
-    public void Spawn()
+    private int feedAmountMin, feedAmountMax, spawnTimerMin, spawnTimerMax;
+    private int randomValue;
+    [SerializeField] private GameObject monster;
+    [SerializeField] private Camera cam;
+    
+    private void Start()
     {
-
+        StartCoroutine(SpawnCheck());
+        feedAmountMin = 5;
+        feedAmountMax = 15;
+        spawnTimerMin = 30;
+        spawnTimerMax = 120;
     }
+
+    public IEnumerator Spawn()
+    {
+        
+        feedAmount = Random.Range(feedAmountMin, feedAmountMax);    
+        monster.SetActive(true);
+        yield return null;
+    }
+    private IEnumerator SpawnCheck()
+    {
+        while (true)
+        {
+
+            if (shouldAppear)
+            {
+                randomValue = Random.Range(0, spawnChance);
+            }
+
+            yield return new WaitForSeconds(spawnTimer);
+            spawnTimer = Random.Range(spawnTimerMin, spawnTimerMax);
+            if (randomValue == 0)
+            {
+
+                if (shouldAppear)
+                {
+                    StartCoroutine(Spawn());
+                }
+                yield return null;
+            }
+            else
+            {
+                spawnChance--;
+                yield return null;
+            }
+
+        }
+    }
+
+
 }
+
