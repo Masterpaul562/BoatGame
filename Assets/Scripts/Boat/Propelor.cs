2418d60397a8383d459a3315f77991d59bbbec9f
Masterpaul562 <178920367+Masterpaul562@users.noreply.github.com>
2025-12-16 10:41:00 -0700

    propelor

---
+++ Assets/Scripts/Boat/Propelor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propelor : MonoBehaviour
+{
+    private SpriteRenderer render;
+    private SpriteMask mask;
+
+
+
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+        mask = GetComponent<SpriteMask>();
+    }  
+    void Update()
+    {
+        mask.sprite = render.sprite;
+    }
+}
