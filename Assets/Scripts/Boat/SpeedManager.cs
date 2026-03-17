8dc943e09e48289744281a9e061477153265f988
Masterpaul562 <9652749@graniteschools.org>
2026-03-16 21:12:36 -0600

    earwiggy speed started

--- Assets/Scripts/Boat/SpeedManager.cs
+++ Assets/Scripts/Boat/SpeedManager.cs
@@ -7,13 +7,19 @@ public class SpeedManager : MonoBehaviour
     public float currentSpeed;
     public FishEngineReal engine;
 
+
+    [Header("Earwig")]
+    public float earwigSpeed;
+    public float earwigDistance;
+    public float maxEarwigDistance;
+
     [Header("Speed Modifier")]
     public int stageKnotAmount;
         
 
     void Start()
     {
-      
+      StartCoroutine(EarwigMove()); 
     }
 
 
@@ -27,5 +33,22 @@ public class SpeedManager : MonoBehaviour
         currentSpeed = 0;
         currentSpeed += engine.powerStage * stageKnotAmount;
         currentSpeed += (stageKnotAmount * 3 ) * engine.powerSet;
+    }
+    private IEnumerator EarwigMove()
+    {
+        while (true)
+        {
+            if (currentSpeed < earwigSpeed)
+            {
+                earwigDistance = Mathf.MoveTowards(earwigDistance, 0, Time.deltaTime * (earwigSpeed - currentSpeed));
+                
+
+            }
+            else
+            {
+                earwigDistance = Mathf.MoveTowards(earwigDistance, maxEarwigDistance, Time.deltaTime* (currentSpeed - earwigSpeed));
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
