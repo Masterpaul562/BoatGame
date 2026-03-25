823034d3144adb3e7eb96aea35b30cc203d27bec
Masterpaul562 <9652749@graniteschools.org>
2026-03-24 21:06:27 -0600

    level manager

--- Assets/Scripts/World/LevelManager.cs
+++ Assets/Scripts/World/LevelManager.cs
@@ -4,6 +4,40 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    [Header("Level Info")]
     public int currentLevel;
+    public float levelDistance;
 
+    [Header("Settings")]
+    public float maxLevelDistance;
+
+    [Header("")]
+    [SerializeField] private SpeedManager speed;
+
+    private void Start()
+    {
+        currentLevel = 1;
+        levelDistance = maxLevelDistance;
+    }
+
+    private void Update()
+    {
+        MoveTowardsLevel();
+    }
+
+
+    private void MoveTowardsLevel()
+    {
+         levelDistance = Mathf.MoveTowards(levelDistance, 0f, Time.deltaTime* speed.currentSpeed);
+        if(levelDistance <= 0f)
+        {
+            levelDistance = maxLevelDistance;
+            ChangeLevel(currentLevel+1);
+        }
+    }
+
+    private void ChangeLevel(int levelNum)
+    {
+        currentLevel = levelNum;
+    }
 }
