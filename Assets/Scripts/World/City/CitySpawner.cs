55c238cf1a25d9c3bb99c1cb6e9c3c4c6481b615
Masterpaul562 <9652749@graniteschools.org>
2026-03-30 20:13:05 -0600

    city spawning

--- Assets/Scripts/World/City/CitySpawner.cs
+++ Assets/Scripts/World/City/CitySpawner.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class CitySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private LevelManager level;
+
+    private bool spawnedCity;
+
+    [SerializeField] private  GameObject city;
+
+    private void Start()
     {
-        
+        level = this.GetComponent<LevelManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (level.currentLevel == 2 && !spawnedCity)
+        {
+            SpawnCity();
+        }else if (level.currentLevel >= 3)
+        {
+            DespawnCity();
+        }
+    }
+
+    private void SpawnCity()
+    {
+        spawnedCity = true;
+        city.SetActive(true);
+    }
+
+    private void DespawnCity()
+    {
+        spawnedCity = false;
+        city.SetActive(false);
     }
 }
