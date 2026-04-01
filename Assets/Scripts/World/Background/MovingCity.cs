51869dfff4c99ece0440f3f77ff89de163f52b97
Masterpaul562 <9652749@graniteschools.org>
2026-03-31 20:26:28 -0600

    city moving

---
+++ Assets/Scripts/World/Background/MovingCity.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCity : MonoBehaviour
+{
+    [Range(-1, 1)] public int direction;
+    public float speed;
+
+    private float x;
+
+
+    private void Update()
+    {
+        MoveCity();
+    }
+
+    private void MoveCity()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, Vector2.left, Time.deltaTime * direction * speed) ;
+    }
+}
