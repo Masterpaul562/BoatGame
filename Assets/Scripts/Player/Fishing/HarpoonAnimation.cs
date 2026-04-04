a05091896fc7a26af0e5c3ca397c94a860ee1671
Masterpaul562 <9652749@graniteschools.org>
2026-04-03 18:00:09 -0600

    harpoon firing

--- Assets/Scripts/Player/Fishing/HarpoonAnimation.cs
+++ Assets/Scripts/Player/Fishing/HarpoonAnimation.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HarpoonStow : MonoBehaviour
+public class HarpoonAnimation : MonoBehaviour
 {
     [SerializeField] private HarpoonGun2 harpoon;
 
     public void Stow()
     {
         harpoon.StowHarpoon();
+    }
+    public void Fire()
+    {
+        harpoon.Fire();
     }
 }
 
--- Assets/Scripts/Player/Fishing/HarpoonGun2.cs
+++ Assets/Scripts/Player/Fishing/HarpoonGun2.cs
@@ -7,20 +7,24 @@ public class HarpoonGun2 : MonoBehaviour
     [Header("Refrences")]
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject harpoon;
-    [SerializeField] private GameObject bobber;
     [SerializeField] private GameObject line;
+    [SerializeField] private GameObject harpHead;
+    [SerializeField] private Transform harpEnd;
     private Animator anim;
 
     [Header("Settings")]
+    [SerializeField] private int power;
     public float rotationSpeed;
     public KeyCode inputKey;
     public KeyCode fireKey;
     private bool canCast = true;
+    private bool canFire = false;
     private Quaternion ogRotation;
+    private float distance;
 
     [Header("Info")]
-    [SerializeField] private int powerLevel;
     public bool isFishing;
+    public bool hasFire;
 
     private void Start()
     {
@@ -40,13 +44,20 @@ public class HarpoonGun2 : MonoBehaviour
             anim.SetTrigger("Stow");
         }
 
+        if(canFire && Input.GetKeyDown(fireKey))
+        {
+            anim.SetTrigger("Fire");
+        }
         anim.SetFloat("Speed", player.GetComponent<PlayerMove>().animator.GetFloat("Speed"));
+
+        ReelCheck();
     }
 
     private void PrepHarpoon()
     {
         isFishing = true;
         canCast = false;
+        canFire = true;
         harpoon.SetActive(true);
         anim.SetTrigger("Prep");
     }
@@ -55,16 +66,54 @@ public class HarpoonGun2 : MonoBehaviour
     {
         isFishing = false;  
         canCast = true;
+        canFire= false;
+        line.SetActive(false);
         harpoon.SetActive(false);
         harpoon.transform.rotation = ogRotation;
     }
 
-    private void Fire()
+    public void Fire()
     {
+        line.SetActive(true);
+        harpHead.GetComponent<Rigidbody2D>().simulated = true;
+        hasFire = true;
 
+        if (player.GetComponent<Animator>().GetBool("isFacingRight"))
+        {
+            harpHead.GetComponent<Rigidbody2D>().AddForce(harpoon.transform.right * power, ForceMode2D.Impulse);
+        }
+        else
+        {
+            harpHead.GetComponent<Rigidbody2D>().AddForce((harpoon.transform.right * power) * -1, ForceMode2D.Impulse);
+        }
+    }
+
+    public IEnumerator Reel()
+    {
+        harpHead.GetComponent<Rigidbody2D>().simulated = false;
+
+        distance = 100f;
+        while (distance > 0.1f)
+        {
+            Vector2 pos = Vector2.MoveTowards(harpHead.transform.position, harpEnd.position, Time.deltaTime * 20);
+            harpHead.transform.position = pos;
+            distance = Vector2.Distance(harpEnd.position, harpHead.transform.position);
+            yield return null;
+        }
+        anim.SetTrigger("Reeled");
+        line.SetActive(false);
+        hasFire = false;
+        yield return null;
     }
     
-
+    private void ReelCheck()
+    {
+        if (Vector2.Distance(harpEnd.position, harpHead.transform.position) > 7)
+        {
+            StopAllCoroutines();
+            StartCoroutine(Reel());
+        }
+    }
     private void SetInside()
     {
         var inside = player.GetComponent<EnterBoat>();
