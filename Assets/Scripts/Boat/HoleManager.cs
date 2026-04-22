ffa0045ce8680c9db854bdc49bd39d830ec5f452
Masterpaul562 <178920367+Masterpaul562@users.noreply.github.com>
2026-04-22 10:43:01 -0600

    show hole

--- Assets/Scripts/Boat/HoleManager.cs
+++ Assets/Scripts/Boat/HoleManager.cs
@@ -8,16 +8,29 @@ public class HoleManager : MonoBehaviour
 
     [Header("Refrences")]
     [SerializeField] private GameObject holePrefab;
-    [SerializeField] private SpriteRenderer spawnLocation;
+    [SerializeField] private Collider2D spawnLocation;
+    [SerializeField] private Transform boat;
 
     private void Update()
     {
-        Debug.Log(spawnLocation.bounds.size.x);
+        if(Input.GetMouseButtonDown(0)){
+CreateHole();
+        }
     }
 
     public void CreateHole()
     {
+       Vector2 spawnPos = FindSpawnLocation(spawnLocation.bounds);
+        Instantiate(holePrefab,spawnPos, Quaternion.identity, boat);
 
+    }
+
+    private Vector2 FindSpawnLocation(Bounds bound)
+    {
+        return new Vector2 (
+        Random.Range(bound.min.x,bound.max.x),
+        Random.Range(bound.min.y,bound.max.y)
+        );
     }
 
 }
