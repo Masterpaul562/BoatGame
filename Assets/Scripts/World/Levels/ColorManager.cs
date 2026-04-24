8e5982bb20b1b1b5df9ca819ce185edc6872d5c1
Masterpaul562 <9652749@graniteschools.org>
2026-04-23 21:50:30 -0600

    started color

---
+++ Assets/Scripts/World/Levels/ColorManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    [SerializeField] private Color currentColor;
+
+    [Header("LevelColors")]
+    public Color industrialZoneColor;
+    public Color cityColor;
+    public Color theProjectColor;
+    public Color infestedZone;
+
+    [Header("ObjectsToChange")]
+    public SpriteRenderer[] objects;
+
+    [Header("Settings")]
+    public float shiftSpeed;
+
+
+
+    private void Update()
+    {
+        ColorShift(infestedZone);
+    }
+
+    public void ColorShift(Color targetColor)
+    {
+        Vector3 color = new Vector3(currentColor.r, currentColor.g, currentColor.b);
+        Vector3 newColor = new Vector3(targetColor.r, targetColor.g, targetColor.b);
+        color = Vector3.MoveTowards(color, newColor, Time.deltaTime * shiftSpeed);
+        currentColor = new Color(color.x,color.y,color.z);
+    }
+}
