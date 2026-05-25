4a0b435be79e48f0a0f8b296d89d34b44bf1cd73
Masterpaul562 <9652749@graniteschools.org>
2026-05-24 20:54:13 -0600

    sprite cuts and fixing hole animation

--- Assets/Scripts/Boat/HoleManager.cs
+++ Assets/Scripts/Boat/HoleManager.cs
@@ -152,6 +152,7 @@ public class HoleManager : MonoBehaviour
                 int index = FindHole(hit.collider.gameObject);
 
                 player.GetComponent<PlayerMove>().freeze = true;
+                player.GetComponent<Rigidbody2D>().velocity = new Vector2 (0,0);
                 player.GetComponent<Animator>().SetTrigger("FixHole");
 
                 Destroy(holes[index]);
